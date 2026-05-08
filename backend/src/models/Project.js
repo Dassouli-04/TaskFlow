ca3781b960632f9aab0c8cafc268a42e16a4a55f
mouhsine daghmoumy <mouhsinemouhsine76@gmail.com>
2026-05-08 13:23:28 +0100

    fix server.js syntax error . backend working on port 5000

---
+++ backend/src/models/Project.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Task = require("./Task");
+
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 150
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    deadline: {
+      type: Date,
+      default: null
+    },
+
+    status: {
+      type: String,
+      enum: ["actif", "en pause", "archivé"],
+      default: "actif"
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+projectSchema.pre("deleteOne", { document: true, query: false }, async function () {
+    await Task.deleteMany({ project: this._id });
+ });
+
+module.exports = mongoose.model("Project", projectSchema);
